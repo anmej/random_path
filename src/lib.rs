22f3d79efb1b2df240c41f466bfdae8612c3c323
anmej <no>
2016-11-16 14:54:18 +0200

    more

--- src/lib.rs
+++ src/lib.rs
@@ -7,43 +7,52 @@ pub struct Point {
     pub y: i32,
 }
 
+#[derive(Debug)]
 pub struct PathBuilder {
     pub width: i32,
     pub heigth: i32,
-    pub minimal_path_length: i32,
+    pub minimal_path_len: i32,
     pub start: Point,
     pub end: Point,
     pub blacklist: Vec<Point>, // list of locations that lead to dead end
     pub path: Vec<Point>,
+    pub return_len: usize, // number of steps to retreat when dead-end
 }
 
 impl PathBuilder {
     pub fn new(width: i32,
                heigth: i32,
-               minimal_path_length: i32,
+               minimal_path_len: i32,
                start: Point,
                end: Point)
-               -> PathBuilder {
+               -> PathBuilder {  
         let mut b = PathBuilder {
             width: width,
             heigth: heigth,
-            minimal_path_length: minimal_path_length,
+            minimal_path_len: minimal_path_len,
             start: start,
             end: end,
             blacklist: vec![],
             path: vec![],
+            return_len: 0,
         };
+        if !b.is_valid(b.start) {
+            panic!("Start point is invalid")
+        } 
+        if !b.is_valid(b.end) {
+            panic!("End point is invalid")
+        }
         b.path.push(b.start);
         b
     }
 
     pub fn is_valid(&self, point: Point) -> bool {
-        point.x > 0 && point.x < self.width && point.y > 0 && point.y < self.heigth
+        point.x >= 0 && point.x < self.width && point.y >= 0 && point.y < self.heigth
     }
 
     pub fn is_free(&self, point: Point) -> bool {
         // while path is too small, path.tend is not free
-        if self.path.len() < self.minimal_path_length as usize {
+        if self.path.len() < self.minimal_path_len as usize {
             !self.blacklist.contains(&point) && !self.path.contains(&point) &&
             self.is_valid(point) && point != self.end
         } else {
@@ -53,8 +62,8 @@ impl PathBuilder {
     }
     // point which is free and has 3 free neighbours (the 4th is the current path end)
     pub fn is_walkable(&self, point: Point) -> bool {
-        //path.end is always walkable when path is long enough
-        if point == self.end && self.path.len() > self.minimal_path_length as usize {
+        // path.end is always walkable when path is long enough
+        if point == self.end && self.path.len() > self.minimal_path_len as usize {
             true
         } else {
             self.is_free(point) && self.get_free_neighbours(point).len() == 3
@@ -91,22 +100,38 @@ impl PathBuilder {
 
     pub fn walk(&mut self) -> bool {
         // end condition
-        if self.path.len() >= self.minimal_path_length as usize &&
-           is_neighbours(*self.path.last().unwrap(), self.end) {
+        if self.path.len() >= self.minimal_path_len as usize &&
+           is_neighbours(*self.path.last().expect("walk: self.path is empty 1"),
+                         self.end) {
             return false;
         }
         // self.path is never empty, new() pushes start into it
-        let walkable_neighbours = self.get_walkable_neighbours(*self.path.last().unwrap());
+
+        println!("self.path: {:?}", self.path);
+
+        let walkable_neighbours =
+            self.get_walkable_neighbours(*self.path.last().expect("walk: self.path is empty 2"));
+
+        println!("walkable neighbours: {:?}", walkable_neighbours);
+
         if walkable_neighbours.len() > 0 {
+            if walkable_neighbours.len() == 1 {
+                self.return_len += 1;
+            } else {
+                self.return_len = 1;
+            }
             // choose one at random and advance
             let next_step = rand::thread_rng().choose(walkable_neighbours.as_slice());
             self.path.push(*next_step.unwrap());
         } else {
             // nowhere yt
-            unimplemented!()
+            self.return_len += 1;
+            for _ in 0..self.return_len {
+                let r = self.path.pop().unwrap();
+                self.blacklist.push(r);
+            }
+            self.return_len = 1;
         }
-
-
         true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@ use lib::*;
 
 fn main() {
     let a = Point { x: 0, y: 0 };
-    let b = Point { x: 4, y: 4 };
+    let b = Point { x: 0, y: 0 };
     let mut state = PathBuilder::new(5, 5, 5, a, b);
-    let c = Point { x: 1, y: 1 };
-    state.path.push(c);
+    // for i in 0..100 {
+    //     let x = state.walk();
+        
+    //     println!{"cycle: {}", i};
+    //     println!{"path: {:?}", state.path};
+    //     println!{"blacklist: {:?}", state.blacklist};
+        
+    //     if !x {
+    //         break;
+    //     }
+    // }
+    let p = Point{x: 1, y: 0};
+    println!{"f.n.: {:?}", state.get_free_neighbours(p)};
+    println!{"w.n.: {:?}", state.get_walkable_neighbours(p)};
+    
 }
